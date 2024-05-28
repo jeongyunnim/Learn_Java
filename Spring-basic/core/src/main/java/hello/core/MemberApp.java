ac8b9a41a5edb51e90966780d87d85243553b76c
jeongyunnim <jeongyun7521@gmail.com>
2024-05-28 18:00:57 +0900

    refactor: 관심사 분리

--- Spring-basic/core/src/main/java/hello/core/MemberApp.java
+++ Spring-basic/core/src/main/java/hello/core/MemberApp.java
@@ -7,10 +7,13 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+//        MemberService memberService = new MemberServiceImpl();
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-        
+
         Member findMember = memberService.findMember(1L);
         System.out.println("findMember = " + findMember.getName());
         System.out.println("Member = " + member.getName());
