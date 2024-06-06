826e40db10fe7822d9d8dcb2dcbf2fb76f716885
jeongyunnim <jeongyun7521@gmail.com>
2024-06-07 01:12:57 +0900

    feat: lombok 적용

---
+++ Spring-basic/core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,9 @@
+package hello.core.order;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.*;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
