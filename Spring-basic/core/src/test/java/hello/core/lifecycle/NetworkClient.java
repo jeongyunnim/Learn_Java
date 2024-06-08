0c3d67d7d0b195ed72330f3bebaf7606074b0cdc
jeongyunnim <jeongyun7521@gmail.com>
2024-06-08 16:22:14 +0900

    feat: Bean 생명주기 콜백 에제 작성 완료

---
+++ Spring-basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,33 @@
+package hello.core.lifecycle;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.Setter;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+@Setter
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출 url = " + url);
+    }
+
+    //서비스 시작 시 호출
+    public void connect() {
+
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+ 
+}
