c9d1dde41985e44f2b7e3ccd6d166745394de649
jeongyunnim <jeongyun7521@gmail.com>
2024-06-09 00:51:20 +0900

    feat: 핵심편 작성 완료

--- Spring-basic/core/src/main/java/hello/core/web/LogDemoController.java
+++ Spring-basic/core/src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,10 +18,12 @@ public class LogDemoController {
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        // 자바에서 제공하는 표준 서블렛 규약에 의한 고객의 요청 정보를 조회할 수 있다.
         myLogger.setRequestURL(requestURL);
+
+        System.out.println("myLogger = " + myLogger);
+
         myLogger.log("controller test");
-        logDemoService.logic("testid");
+        logDemoService.logic("testId");
         return "OK";
     }
 }
