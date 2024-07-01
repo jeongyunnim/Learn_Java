3267c1b3489bd79e8445e1eb295503ec0d03c23b
jeongyunnim <jeongyun7521@gmail.com>
2024-07-01 16:09:50 +0900

    feat: SCG 서버 설정

---
+++ Spring-MSA/SCG/src/test/java/config/example/scg/ScgApplicationTests.java
@@ -0,0 +1,13 @@
+package config.example.scg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
