f4cc6ecdb26d58af70c9c6df92774df3b3348058
jeongyunnim <jeongyun7521@gmail.com>
2024-07-11 14:34:41 +0900

    feat: swagger ui 정보를 추가적으로 전달

--- Spring-MSA-Udemy/accounts/src/main/java/com/easybytes/accounts/constants/AccountsConstants.java
+++ Spring-MSA-Udemy/accounts/src/main/java/com/easybytes/accounts/constants/AccountsConstants.java
@@ -11,6 +11,7 @@ public class AccountsConstants {
     public static final String MASSAGE_201 = "Account create successfully";
     public static final String STATUS_200 = "200";
     public static final String MASSAGE_200 = "Request processed successfully";
-    public static final String STATUS_500 = "500";
-    public static final String MASSAGE_500 = "An error occurred. Please try again or contact Dev team";
+    public static final String STATUS_417 = "417";
+    public static final String MASSAGE_417_UPDATE = "Update Operation failed. Please try again or contact Dev team";
+    public static final String MASSAGE_417_DELETE = "Delete Operation failed. Please try again or contact Dev team";
 }
--- Spring-MSA-Udemy/accounts/src/main/java/com/easybytes/accounts/dto/ResponseDto.java
+++ Spring-MSA-Udemy/accounts/src/main/java/com/easybytes/accounts/dto/ResponseDto.java
@@ -1,11 +1,22 @@
 package com.easybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@Schema(
+        name = "Response",
+        description = "Schema to hole successful response information"
+)
 @Data @AllArgsConstructor
 public class ResponseDto {
+    @Schema(
+            description = "Status code in the response", example = "200"
+    )
     private String statusCode;
 
+    @Schema(
+            description = "Status message in the response", example = "Request processed successfully"
+    )
     private String statusMsg;
 }
