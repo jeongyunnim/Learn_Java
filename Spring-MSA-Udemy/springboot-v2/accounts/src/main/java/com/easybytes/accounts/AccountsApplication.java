242b8f5edc0a2ca4aa4e0cd7b3468af2cf15af63
jeongyunnim <jeongyun7521@gmail.com>
2024-07-17 18:22:49 +0900

    feat: config server를 활용한 예제 구성

---
+++ Spring-MSA-Udemy/springboot-v2/accounts/src/main/java/com/easybytes/accounts/AccountsApplication.java
@@ -0,0 +1,43 @@
+package com.easybytes.accounts;
+
+import com.easybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "Easy Bank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Jeseo",
+                        email = "Jeseo@test.com",
+                        url = "https://www.naver.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "https://www.tistory.com"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Easy Bank Accounts microservice REST API Documentation",
+                url = "https://www.eazybytes.com"
+        )
+)
+public class AccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}
