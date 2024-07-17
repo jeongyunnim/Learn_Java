d249997d40cf77c100844799de2417b2fe83460f
jeongyunnim <jeongyun7521@gmail.com>
2024-07-17 18:10:50 +0900

    feat: spring boot profile 설정

--- Spring-MSA-Udemy/accounts/src/main/java/com/easybytes/accounts/dto/AccountsContactInfoDto.java
+++
@@ -1,9 +0,0 @@
-package com.easybytes.accounts.dto;
-
-import java.util.List;
-import java.util.Map;
-
-// record: 읽기만 가능한 자료형, 기본적으로 getter를 제공하며 변경이 불가능하다.
-public record AccountContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
-
-}
