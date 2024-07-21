0308e663593093988be19d07602f2fe4a5a6e243
jeongyunnim <jeongyun7521@gmail.com>
2024-07-21 18:57:39 +0900

    chore: 변경된 정보를 BUS를 사용하여 모든 서비스 동기화 설정 완료

--- Spring-MSA-Udemy/springboot-v2/accounts/src/main/java/com/easybytes/accounts/dto/AccountsContactInfoDto.java
+++ Spring-MSA-Udemy/springboot-v2/accounts/src/main/java/com/easybytes/accounts/dto/AccountsContactInfoDto.java
@@ -1,12 +1,19 @@
 package com.easybytes.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-// record: 읽기만 가능한 자료형, 기본적으로 getter를 제공하며 변경이 불가능하다.
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 
 }
