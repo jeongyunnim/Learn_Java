6307c240597a6fb27855828a0df21f62087ce683
jeongyunnim <jeongyun7521@gmail.com>
2024-07-29 20:18:12 +0900

    feat: scg 프로젝트 생성
    1. 사용자 정의 라우팅 정의 완료

---
+++ Spring-MSA-Udemy/scg(v9)/accounts/src/main/java/com/easybytes/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,17 @@
+package com.easybytes.accounts.service.client;
+
+import com.easybytes.accounts.dto.CardsDto;
+import com.easybytes.accounts.dto.LoansDto;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans") // eureka 서버에 등록된 이름으로 해당 정보를 받아온다.
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch", consumes = "application/json")
+    public ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam String mobileNumber);
+
+}
