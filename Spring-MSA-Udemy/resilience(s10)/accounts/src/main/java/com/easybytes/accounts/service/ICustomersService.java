8280d326a8ea890ecfa82e48d133e096277f5b19
jeongyunnim <jeongyun7521@gmail.com>
2024-07-31 14:56:58 +0900

    feat: section 10 예제 추가

---
+++ Spring-MSA-Udemy/resilience(s10)/accounts/src/main/java/com/easybytes/accounts/service/ICustomersService.java
@@ -0,0 +1,13 @@
+package com.easybytes.accounts.service;
+
+import com.easybytes.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
+}
