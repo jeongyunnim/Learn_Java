3e29b8771aee3a8a3814c4e4f52b7043056a24c2
jeongyunnim <jeongyun7521@gmail.com>
2024-08-13 18:08:33 +0900

    feat: Tempo 설정 추가
    모든 기능 정상 작동 확인

--- Spring-MSA-Udemy/Observability&Monitoring(s11)/accounts/src/main/java/com/easybytes/accounts/controller/CustomerController.java
+++ Spring-MSA-Udemy/Observability&Monitoring(s11)/accounts/src/main/java/com/easybytes/accounts/controller/CustomerController.java
@@ -51,8 +51,10 @@ public class CustomerController {
     public ResponseEntity<CustomerDetailsDto> fetchCustomerDetails(@RequestHeader("eazybank-correlation-id") String correlationId,
                                                                    @RequestParam @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
                                                                    String mobileNumber) {
-        logger.debug("eazyBank-correlation-id: {}", correlationId);
-        return ResponseEntity.status(HttpStatus.OK).body(customerService.fetchCustomerDetails(mobileNumber, correlationId));
+        logger.debug("fetchCustomerDetails method start");
+        CustomerDetailsDto customerDetailsDto = customerService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails method end");
+        return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
     }
 
 }
